47f01ed30d7f56fdd59c793734091063b35adae7
keepsimpler <fengwenfeng@gmail.com>
2014-04-10 19:25:35 +0900

    feasible states and nestedness

--- DE.R
+++ DE.R
@@ -11,7 +11,7 @@ library(rootSolve)
 #' @return the derivation
 #' @details .
 #' @import deSolve
-lv.1 <- function(t, N, parms) {
+lv1 <- function(t, N, parms) {
   r = parms[[1]]
   M = parms[[2]]
   dN <- N * (r + M %*% N)
@@ -27,30 +27,81 @@ lv.1 <- function(t, N, parms) {
 #' betaP,betaA the intraspecific and interspecific competition interaction
 #' among species in same groups, Plants group or Animal group.
 #' gammaP,gammaA the mutualistic interaction between Plants and Animal.
-#' h the Handling time, saturate coefficient.
+#' hP,hA the Handling time, saturate coefficient.
 #' @return the derivation
 #' @details .
 #' @import deSolve
-lv.2 <- function(t, N, parms) {  # Times, State variables, parameters
+lv2 <- function(t, N, parms) {  # Times, State variables, parameters
   alphaP = parms[[1]]
   alphaA = parms[[2]]
   betaP = parms[[3]]
   betaA = parms[[4]]
   gammaP = parms[[5]]
   gammaA = parms[[6]]
-  h = parms[[7]]
+  hP = parms[[7]]
+  hA = parms[[8]]
   
   numP = length(alphaP)
   numA = length(alphaA)
   NP = N[1:numP]
   NAA = N[(numP + 1) : (numP + numA)]
-  dNP = alphaP - betaP %*% NP + gammaP %*% NAA / (1 + h * gammaP %*% NAA)
-  dNA = alphaA - betaA %*% NAA + gammaA %*% NP / (1 + h * gammaA %*% NP)
+  dNP = alphaP - betaP %*% NP + gammaP %*% NAA / (1 + hP * gammaP %*% NAA)
+  dNA = alphaA - betaA %*% NAA + gammaA %*% NP / (1 + hA * gammaA %*% NP)
   dN = N * c(dNP, dNA)
   list(c(dN))
 }
 
-lv.1.check <- function(dataset,  # the network
+###############################################################################
+#' @title Check the steady state of LV2 model
+#' @references 'the sudden collapse of pollinator communities'
+#' @param dataset the incidence matrix of bipartite network to be checked
+#' @param extinct.threshold used to decide if species has extinct
+#' @return a list with:
+#' @return survived  the number of survived species when in steady state
+#' @return extinct  the number of extinct species when in steady state
+#' @return lev  the Largest EiganValue of Jacobian matrix in steady state
+#' @details .
+lv2.check <- function(dataset, extinct.threshold = 10^-8) {
+  dataset[dataset > 0] = 1  # insure input is a binary network
+  numP = dim(dataset)[1]  # number of Plants
+  numA = dim(dataset)[2]  # number of Animals
+
+  ## Generate Parameters for LV2 model by sampling from uniform distributions
+  alphaP = runif(numP) * 0.3 + 0.05  # Intrinsic growth, uniformly in [0.05, 0.35]
+  alphaA = runif(numA) * 0.3 + 0.05  # Intrinsic growth, uniformly in [0.05, 0.35]
+  
+  betaP = matrix(runif(numP * numP), ncol = numP) * 0.04 + 0.01  # Interspecific competition, uniformly in [0.01, 0.05]
+  diagP = runif(numP) * 0.3 + 0.8  # Intraspecific competition, uniformly in [0.8, 1.1]
+  diag(betaP) = diagP
+  betaA = matrix(runif(numA * numA), ncol = numA) * 0.04 + 0.01  # Interspecific competition, uniformly in [0.01, 0.05]
+  diagA = runif(numA) * 0.3 + 0.8  # Intraspecific competition, uniformly in [0.8, 1.1]
+  diag(betaA) = diagA
+  
+  gammaP = matrix(runif(numP * numA), ncol = numA) * 0.4 + 0.8  # Mutualistic strength between Plants and Animals, uniformly in [0.8, 1.2]
+  gammaP = gammaP * dataset
+  gammaA = matrix(runif(numA * numP), ncol = numP) * 0.4 + 0.8  # Mutualistic strength between Plants and Animals, uniformly in [0.8, 1.2]
+  gammaA = gammaA * t(dataset)
+  
+  hP = runif(numP) * 0.15 + 0.15  # Saturation parameter, uniformly in [0.15, 0.3]
+  hA = runif(numA) * 0.15 + 0.15  # Saturation parameter, uniformly in [0.15, 0.3]
+  
+  parms = list(alphaP, alphaA, betaP, betaA, gammaP, gammaA, hP, hA)  # parameters
+  t = seq(1, 500)  # times
+  N0 = runif(numP + numA) + 0.5  # Initial species abundence, uniformly in [0.5, 1.5]
+  
+  lvout <- ode(y = N0, t, func = lv2, parms = parms, atol = 10^-14, rtol = 10^-12)  # 
+  Nstar = lvout[nrow(lvout), 2:ncol(lvout)]  # the species abundance at steady state
+  Nstar[Nstar < extinct.threshold] = 0  # species with biomass less than the threshold is considered to be extinct
+  
+  comm = jacobian.full(y = Nstar, func = lv2, parms = parms)  # Jacobian matrix in equilibrium
+  lev = max(Re(eigen(comm)$values))
+  
+  survived = sum(Nstar > 0)  # Survived species at equillibrim state
+  extinct = sum(Nstar == 0)  # Extinct species at equillibrim state
+  list(survived = survived, extinct = extinct, lev = lev)
+}
+
+lv1.check <- function(dataset,  # the network
                        stry = 100,  # number of try for randomizing Structure
                        ptry = 100)  # number of try for Parameterization 
 {
@@ -68,7 +119,7 @@ lv.1.check <- function(dataset,  # the network
     nodf = nestednodf(dataset.swap)$statistic['NODF']  # nested(dataset.swap, method = 'NODF2')  # NODF of the swapped network
     nest.cmnb = nest.cmnb(dataset.swap)['CMNB']
     for (parameterization in 1:ptry) {
-      N0 = runif(numP + numA)  # Initial species abundence, uniformly in (0, 1)
+      N0 = runif(numP + numA) + 0.5  # Initial species abundence, uniformly in [0.5, 1.5]
       alphaP = runif(numP) * 0.25 + 0.85  # Intrinsic growth, uniformly in [0.85, 1.1]
       alphaA = runif(numA) * 0.25 + 0.85  # Intrinsic growth, uniformly in [0.85, 1.1]
       
@@ -89,14 +140,21 @@ lv.1.check <- function(dataset,  # the network
       M = rbind(cbind(-betaP, gammaP), cbind(gammaA, -betaA))
       parms = list(r, M)  # parameters
       
-      lvout <- ode(y = N0, t, func = lv.1, parms = parms)  # , atol = 10^-14, rtol = 10^-12
-      lvout.2 <- stode(y = N0, fun = lv.1, parms = parms, pos = TRUE)  # 
-      lvout.3 <- runsteady(y = N0, fun = lv.1, parms = parms, times = c(0, 1e5))
-      comm = jacobian.full(y = lvout[nrow(lvout), 2:ncol(lvout)], func = lv.1, parms = parms)
+      lvout <- ode(y = N0, t, func = lv1, parms = parms, atol = 10^-14, rtol = 10^-12)  # 
+      extinct.threshold = 10^-8  # 
+      Nstar = lvout[nrow(lvout), 2:ncol(lvout)]  # the species abundance at steady state
+      Nstar[Nstar < extinct.threshold] = 0  # species with biomass less than the threshold is considered to be extinct
+      
+      #lvout.2 <- stode(y = N0, fun = lv1, parms = parms, 
+      #                 atol = 10^-14, rtol = 10^-12, pos = TRUE)  # 
+      #lvout.3 <- ode(y = lvout.2$y, t, func = lv1, parms = parms, atol = 10^-14, rtol = 10^-12)  # 
+      #      lvout.4 <- runsteady(y = N0, fun = lv1, parms = parms, times = c(0, 1e5))
+
+      comm = jacobian.full(y = Nstar, func = lv1, parms = parms)
       lev = max(Re(eigen(comm)$values))
       
-      survived = sum(lvout[nrow(lvout), 2:ncol(lvout)] >= 10^-8)  # Survived species at equillibrim state
-      extinct = sum(lvout[nrow(lvout), 2:ncol(lvout)] < 10^-8)  # Extinct species at equillibrim state
+      survived = sum(Nstar > 0)  # Survived species at equillibrim state
+      extinct = sum(Nstar == 0)  # Extinct species at equillibrim state
       persistence = survived / (numP + numA)  #
       temp = data.frame( magnitude = magnitude, connectance = connectance, 
                          randomization = randomization, parameterization = parameterization, nodf = nodf,
@@ -109,7 +167,10 @@ lv.1.check <- function(dataset,  # the network
 
 ###############################################################################
 ### Check the persistence of ODE Equations by Bastolla et al.
-lv.2.check <- function(dataset) {
+lv2.check.old <- function(dataset,  # the network
+                       stry = 100,  # number of try for randomizing Structure
+                       ptry = 100)  # number of try for Parameterization 
+{
   result = data.frame( magnitude = numeric(0), connectance = numeric(0), 
                        randomization = numeric(0), parameterization = numeric(0), nodf = numeric(0),
                        survived = numeric(0), extinct = numeric(0), persistence = numeric(0), lev = numeric(0))
@@ -119,12 +180,12 @@ lv.2.check <- function(dataset) {
   magnitude = numP * numA  # network magnitude
   connectance = sum(dataset > 0) / magnitude  # network connectance
   
-  for (randomization in 1:250) { 
+  for (randomization in 1:stry) { 
     dataset.swap = swaplinks(dataset, HowManyToTry = 1000) # randomizing the original network using the swap model
     nodf = nestednodf(dataset.swap)$statistic['NODF']  # nested(dataset.swap, method = 'NODF2')  # NODF of the swapped network
     nest.cmnb = nest.cmnb(dataset.swap)['CMNB']
-    for (parameterization in 1:250) {
-      N0 = runif(numP + numA)  # Initial species abundence, uniformly in (0, 1)
+    for (parameterization in 1:ptry) {
+      N0 = runif(numP + numA) + 0.5  # Initial species abundence, uniformly in [0.5, 1.5]
       alphaP = runif(numP) * 0.25 + 0.85  # Intrinsic growth, uniformly in [0.85, 1.1]
       alphaA = runif(numA) * 0.25 + 0.85  # Intrinsic growth, uniformly in [0.85, 1.1]
       
@@ -139,24 +200,32 @@ lv.2.check <- function(dataset) {
       gammaP = gammaP * dataset.swap
       gammaA = matrix(runif(numA * numP), ncol = numP) * 0.02 + 0.19  # Mutualistic strength between Plants and Animals, uniformly in [0.19, 0.21]
       gammaA = gammaA * t(dataset.swap)
-      
-      h = 0.1  # Saturation parameter
+
+      hP = runif(numP) * 0.1 + 0.05  # Saturation parameter, uniformly in [0.05, 0.15]
+      hA = runif(numA) * 0.1 + 0.05  # Saturation parameter, uniformly in [0.05, 0.15]
       
       t = seq(1, 500)  # times
-      parms = list(alphaP, alphaA, betaP, betaA, gammaP, gammaA, h)  # parameters
+      parms = list(alphaP, alphaA, betaP, betaA, gammaP, gammaA, hP, hA)  # parameters
       
-      lvout <- ode(y = N0, t, func = lv.2, parms = parms)  # , atol = 10^-14, rtol = 10^-12
-      lvout.2 <- stode(y = N0, fun = lv.2, parms = parms)  # , pos = TRUE
-      lvout.3 <- runsteady(y = N0, fun = lv.2, parms = parms, times = c(0, 1e5))
-      comm = jacobian.full(y = lvout[nrow(lvout), 2:ncol(lvout)], func = lv.1, parms = parms)
+      lvout <- ode(y = N0, t, func = lv2, parms = parms, atol = 10^-14, rtol = 10^-12)  # 
+      extinct.threshold = 10^-8  # 
+      Nstar = lvout[nrow(lvout), 2:ncol(lvout)]  # the species abundance at steady state
+      Nstar[Nstar < extinct.threshold] = 0  # species with biomass less than the threshold is considered to be extinct
+      
+      #lvout.2 <- stode(y = N0, fun = lv2, parms = parms, 
+      #                 atol = 10^-14, rtol = 10^-12, pos = TRUE)  # 
+      #lvout.3 <- ode(y = lvout.2$y, t, func = lv2, parms = parms, atol = 10^-14, rtol = 10^-12)  # 
+      # lvout.4 <- runsteady(y = N0, fun = lv2, parms = parms, times = c(0, 1e5))
+      
+      comm = jacobian.full(y = Nstar, func = lv2, parms = parms)
       lev = max(Re(eigen(comm)$values))
       
-      survived = sum(lvout[nrow(lvout), 2:ncol(lvout)] >= 10^-8)  # Survived species at equillibrim state
-      extinct = sum(lvout[nrow(lvout), 2:ncol(lvout)] < 10^-8)  # Extinct species at equillibrim state
+      survived = sum(Nstar > 0)  # Survived species at equillibrim state
+      extinct = sum(Nstar == 0)  # Extinct species at equillibrim state
       persistence = survived / (numP + numA)  #
       temp = data.frame( magnitude = magnitude, connectance = connectance, 
-                        randomization = randomization, parameterization = parameterization, nodf = nodf,
-                        survived = survived, extinct = extinct, persistence = persistence, lev = lev)
+                         randomization = randomization, parameterization = parameterization, nodf = nodf,
+                         survived = survived, extinct = extinct, persistence = persistence, lev = lev)
       result = rbind(result, temp)
     }
   }
--- paper2.test2.R
+++ paper2.test2.R
@@ -1,16 +1,12 @@
+
+
+
 ##### evaluate 'Disentangling nestedness from models of ecological complex'
 
-out = lv.1.check(dataset = Safariland.C.Pre)
 matplot(t, lvout[, 2:10], type = "l", lwd = 1.5, col = 1)
 
 ############ Nestedness definition
 source('nestedness.R')  # import the functions defining different nestednesss
-for (i in datasets.connected.pre) {
-  A = get(i)
-  nest1 = nodf.old(A, order = F)['NODF']
-  nest2 = nestednodf(A,order = F)$statistic['NODF']
-  print(paste(nest1, nest2))
-}
 
 A = Safariland.C.Pre  # incident matrix
 A[A > 0] = 1  # transform to binary matrix
@@ -20,3 +16,5 @@ sum(B2)  # the sum of all the common neighbors between species
 degrees = rowSums(B)  # the degrees (number of neighbors) of species
 degrees^2  # the number of neighbors of neighbors of species
 print(paste(sum(B2), sum(degrees^2)))  # 
+
+
