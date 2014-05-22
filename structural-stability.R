746ec7cffc7d290d551f7ef8e5ad7e8d61dbb05e
keepsimpler <fengwenfeng@gmail.com>
2014-05-22 19:04:09 +0900

    the nestedness increase total abundance, but also increase the variance of different species abundance, and further increase the coefficient of variance.

--- structural-stability.R
+++ structural-stability.R
@@ -24,21 +24,47 @@ AlphaT = t(Alpha) %*% U
 AlphaT == (Lambda * NstarT)
 
 
-# Generate the competition matrix M
+# Generate the cooperation matrix M
 # First, generate the structure
-s = 20
-k = 2
-G = graph.connected(c(s/2, s/2), k = 2, gtype = 'bipartite')
-A = igraph::get.adjacency(G)
-A = as.matrix(A)
+s1 = 100; s2 = 100
+k = 3
+G = graph.connected(c(s1, s2), k = k, gtype = 'bipartite')
+A = igraph::get.incidence(G)
+#A = igraph::get.adjacency(G)
+
+for (i in 1:100) {
+  M = as.one.mode(A)
+  beta0 = - max(s1, s2) 
+  diag(M) = beta0
+  U = eigen(M)$vectors
+  #Alpha = - U[,1]  # intrinsic growth rate parallel with the principal eigenvector (corresponding to the dominant eigenvalue)
+  #Alpha = rep(2, s1 + s2)
+  Alpha = runif(s1 + s2, min = 2, max = 2)
+  Nstar = - solve(M) %*% Alpha  # the feasible fixed point
+  Phi = M * as.vector(Nstar)  # the community matrix
+  abund.mean = mean(Nstar)
+  abund.sd = sd(Nstar)
+  lev = max(eigen(Phi)$values)
+  sev = eigen(Phi)$values[s1+s2-1]
+  levM = max(eigen(M)$values)
+  A = rewirelinks.richer(A, HowManyToTry = 100)
+  print(paste(abund.mean, abund.sd, abund.mean / abund.sd, lev, sev, levM))
+}
+
+
 # Second, appoint values to the links of the structure
-gamma0 = -0.2
-beta0 = 2
+gamma0 = 1
+beta0 = - (s/2 + 0.1)  # ensure the negative diagonal dominant of M, thus the existence of the feasible fixed point
 M = A
 M[M > 0] = gamma0
 diag(M) = beta0
-# Third, 
-# the eigenvalues and eigenvectors of competition matrix
-Lambda = eigen(M)$values
-U = eigen(M)$vectors
+# Third, rewiring links to get more nested structure
+
+
+Alpha = rep(1, s1 + s2)
+Nstar = - solve(M) %*% Alpha
+Phi = M * as.vector(Nstar)  # the community matrix
+# the eigenvalues and eigenvectors of community matrix
+Lambda = eigen(Phi)$values
+U = eigen(Phi)$vectors
 
--- temporal-stability.R
+++ temporal-stability.R
@@ -1,88 +1,45 @@
-# The temporal stability of Multivariate AR(1) model
+library(simecol)
 
-
-#' @title Multivariate AR(1) model simulation
-#' @references IVES, ESTIMATING COMMUNITY STABILITY AND ECOLOGICAL INTERACTIONS FROM TIME-SERIES DATA
-#' @references X[t] = A + B * X[t - 1] + E[t]
-#' @param A, the intrinsic growh rate
-#' @param B, the interaction matrix
-#' @param SigmaE, the covariance matrix of noise (errors)
-#' @param t, times
-#' @param X0, the initial state
-mar1 <- function(A, B, SigmaE, t, X0 = NULL) {
-  if (dim(B)[1] != dim(B)[2])
-    stop('Interaction matrix B should be a square matrix.')
-  m = dim(B)[1]  # dimension of AR model
-  eig = eigen(B, only.values = TRUE)$values
-  if ((any(Mod(eig) > 1)))
-    warning('Unstable AR model.')
-  require(MASS)
-  # m*1 vector of process errors that has a multivariate normal distribution
-  # with mean vector 0 and covariance matrix SigmaE
-  E = mvrnorm(t, rep(0, m), SigmaE)  
-  E = rep(1, t) %*% t(A) + E  # A + E
-  I = diag(1, m)
-  Mu = solve(I - B) %*% A  # mean vector of stationary distribution
-  Mu = as.vector(Mu)
-  if (is.null(X0))  # if initial state values are not provided, use the mean vector of stationary distribution.
-    X0 = Mu
-  X = matrix(0, nrow = 1 + t, ncol = m)
-  X[1, ] = X0
-  for (k in seq(1, t))
-    X[k + 1, ] = X[k, ] %*% B + E[k, ]
-  X
+#' @title the parms of mutualistic lv1 model in soft mean field case
+#' @param A, the incident matrix of mutualistic networks which are bipartite
+#' @param beta0, the mean value of intraspecies competition
+#' @param gamma0, the mean value of interspecies cooperation
+#' @param alpha0, the intrinsic growh rate
+parms.lv1.softmean <- function(A, gamma0 = 1, alpha0 = 1) {
+  numP = dim(A)[1]; numA = dim(A)[2]
+  beta0 = - max(numP, numA)  # ensure the negative diagonal dominant, and thus feasible fixed point
+  require(bipartite)
+  M = as.one.mode(A)  # transform to adjacency matrix
+  M[M > 0] = gamma0
+  diag(M) = beta0
+  r = rep(alpha0, numP + numA)
+  list(r, M)
 }
 
-A=c(1,1)
-B=rbind(c(0.7, -0.7),c(-0.7,0.7))
-m = dim(B)[1]
-SigmaE=rbind(c(1,0),c(0,1))
-t = 10000
-out = mar1(A, B, SigmaE, t, X0 = c(0,0))
-apply(out[1000:t,], 2, mean)
-var(out[1000:t,])
-solve(diag(1,m) - B) %*% A
-solve(diag(1,m^2) - kronecker(B, B)) %*% as.vector(SigmaE)
-plot((t+1-1000):(t+1), out[(t+1-1000):(t+1),2], type = 'l')
-
-sum(apply(out,2,mean)) == mean(apply(out,1,sum)) 
-sum(var(out)) == var(apply(out,1,sum))
-
-MeanX = mean(apply(out,1,sum)) 
-tse = mean(apply(out,1,sum)) / sd(apply(out,1,sum))  # Temporal Stability of Entile community
-tsi = mean(apply(out, 2, mean) / apply(out, 2, sd))  # Temporal Stability of Individual species
-SigmaX = var(out)
-VarX = sum(diag(SigmaX))
-CovX = sum(SigmaX) - VarX
-
-v = -0.5
-m = 5
-B = matrix(rep(v, m^2), ncol = m, nrow = m)
-diag(B) = 1
-I = diag(1, m)
-solve(I - B)
-
-library(plyr)
-ldply(1:15, function(m) {
-  v = -0.5
-  B = matrix(rep(v, m^2), ncol = m, nrow = m)
-  I = diag(1, m)
-  A = (I - B) %*% rep(1, m)
-  SigmaE = diag(1, m)
-  t = 2000
-  out = mar1(A, B, SigmaE, t)
-  out  
-})
-
-for (m in 1:15) {
-  v = -0.5
-  B = matrix(rep(v, m^2), ncol = m, nrow = m)
-  A = (I - B) %*% rep(1, m)
-  SigmaE = diag(1, m)
-  t = 2000
-  out = mar1(A, B, SigmaE, t)
-  
+#' @title Lotka-Volterra (LV) Equations of Holling type I
+#' @param t time
+#' @param N the variables of the LV system, a vector
+#' @param parms parameters passed to LV model
+#' r the intrinsic growth rate of species, a vector
+#' M the interaction matrix between species
+#' @return the derivation
+#' @details .
+#' @import deSolve
+model.lv1 <- function(t, N, parms) {
+  r = parms[[1]]  # intrinsic growth rate
+  M = parms[[2]]  # interaction matrix
+  dN <- N * (r + M %*% N)
+  list(c(dN))
 }
 
 
+LV1 <- odeModel(
+  main = model.lv1, 
+  parms = parms.lv1.softmean(Safariland.C.Pre),
+  times = c(from=1, to=50, by = 1),
+  init = rep(1, 31),
+  solver = 'lsoda')
+
+LV1 <- sim(LV1)
+plot(LV1)
 
